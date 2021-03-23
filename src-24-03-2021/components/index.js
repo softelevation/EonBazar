9397b801eb78e46f3c1b5d842914748b92fe097b
Ankush <ankush.t@idsil.com>
2021-03-24 00:55:38 +0530

    shipping address

--- src-24-03-2021/components/index.js
+++
@@ -1,8 +0,0 @@
-import Block from './Block';
-import Text from './Text';
-import Input from './input';
-import ImageComponent from './ImageComponent';
-import CustomButton from './CustomButton';
-import Button from './Button';
-
-export {Block, Text, Input, ImageComponent, CustomButton, Button};
--- src-24-03-2021/redux/currency/reducer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {combineReducers} from 'redux';
-import currencyDetail from './currencyType/reducer';
-
-const mainDetails = combineReducers({
-  currencyDetail,
-});
-export default mainDetails;
